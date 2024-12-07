27ce5e6f43246ffc8bcb50fb313e4a2d3e9bc871
amare1990 <amaremek@gmail.com>
2024-12-07 16:08:18 +0300

    Implement data loading method

---
+++ src/eda.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+def load_datasets():
+    benin_df = pd.read_csv("../datasets/benin-malanville.csv")
+    sierra_df = pd.read_csv("../datasets/sierraleone-bumbuna.csv")
+    togo_df = pd.read_csv("../datasets/togo-dapaong_qc.csv")
+    return benin_df, sierra_df, togo_df
+
+
