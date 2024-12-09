6bc5be7fbd6339e4c6142976f371f902456b4273
amare1990 <amaremek@gmail.com>
2024-12-09 15:23:21 +0300

    Implement functionality to upload datasets

--- app/main.py
+++ app/main.py
@@ -4,6 +4,22 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Streamlit CSS styling
+st.markdown(
+  """
+<style>
+.title{
+font-size: 27px;
+        font-weight: bold;
+        text-align: center;
+        color: #4CAF50; /* Moonlight Green */
+        margin-bottom: 20px;
+}
+.welcome{
+}
+</style>
+""", unsafe_allow_html=True
+)
 
 # Helper function: Plot correlation heatmap
 
@@ -18,3 +34,12 @@ def plot_scatter_matrix(df, columns):
   fig, ax = plt.subplots(figsize=(12, 10))
   sns.pairplot(df[columns])
   st.pyplot(fig)
+
+st.markdown('<div class="title">Solar Energy Statistical and Exploratory Data Analysis Dashboard</div', unsafe_allow_html=True)
+st.write('Welcome to the MoonLight Energy Solutions dashboard')
+st.write("Explore solar energy data insights for Benin, Togo, and Sierra Leone datasets.")
+
+# File upload section
+st.sidebar.header("Upload a dataset here")
+uploaded_dataset = st.sidebar.file_uploader("Upload a CSV file", type=["CSV"])
+
